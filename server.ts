6436a1bd042e3206e1f1f5a52be6d5e8ac8d31cf
sarzr <arzpourr@gmail.com>
2025-07-17 18:02:34 +0330

    Add dotenv and use in server

--- server.ts
+++ server.ts
@@ -1,6 +1,18 @@
+import dotenv from "dotenv";
+const dotenvConfig = dotenv.config();
+
+if (!!dotenvConfig.error) {
+  console.error(`[-] dotenv config > ${dotenvConfig.error}`);
+
+  console.info("[i] proccess terminated");
+  process.exit(1);
+}
+
+import connectToDatabase from "./database/database-connection";
 import { createServer } from "node:http";
 import { app } from "./app";
 
+connectToDatabase();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
